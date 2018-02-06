81fc3c2dd5f075bcd2c4b75b987649f279d7e5ee
Kimiyuki Shirai <kimiyuki@gmail.com>
2018-02-06 14:54:25 +0900

    fix

--- ga.py
+++ ga.py
@@ -4,79 +4,97 @@ import httplib2
 from apiclient import errors
 from apiclient.discovery import build
 from oauth2client.client import OAuth2WebServerFlow
-
-# Copy your credentials from the console
-#自分の OAuth ID,SECRETがやるのが望ましいです。
-CLIENT_ID = '643412917207-qt8pe5hmntb9dpi5gbis2d3q8aithhhi.apps.googleusercontent.com'
-CLIENT_SECRET = '_UWPT3S0BFH7ONVlzHnNl4ZX'
-
-# Check https://developers.google.com/webmaster-tools/search-console-api-original/v3/ for all available scopes
-OAUTH_SCOPE = ['https://www.googleapis.com/auth/analytics', 
-               'https://www.googleapis.com/auth/analytics.readonly',
-               'https://www.googleapis.com/auth/webmasters.readonly',
-               'https://www.googleapis.com/auth/gmail.send']
-           
-# Redirect URI for installed apps4/4qWAzJo6NbPR-q2M42BbD7oYAFD4mr-mSgoH4OoSID0
-REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
-
-# Run through the OAuth flow and retrieve credentials
-def get_code():
-    flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-    authorize_url = flow.step1_get_authorize_url()
-    print('Go to the following link in your browser: ' + authorize_url)
-    from google.colab import auth
-    #取り消したい場合は、適当な文字を入れて、実行を終わらせ。再度セルの実行をし、文字列を入れなおす
-    return auth.getpass.getpass()
+from google.colab import auth
 
 
-def build_service(code):
-    flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-    credentials = flow.step2_exchange(code.strip())
-    # Create an httplib2.Http object and authorize it with our credentials
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-    ga4 = build('analytics', 'v4', http=http)
-    ga3 = build('analytics', 'v3', http=http)
-    return ga3, ga4
+class GA:
+    def __init__(self):
+        # Copy your credentials from the console
+        #自分の OAuth ID,SECRETがやるのが望ましいです。
+        self.CLIENT_ID = '643412917207-qt8pe5hmntb9dpi5gbis2d3q8aithhhi.apps.googleusercontent.com'
+        self.CLIENT_SECRET = '_UWPT3S0BFH7ONVlzHnNl4ZX'
+        
+        # Check
+        #https://developers.google.com/webmaster-tools/search-console-api-original/v3/ for all scopes
+        OAUTH_SCOPE = ['https://www.googleapis.com/auth/analytics', 
+                       'https://www.googleapis.com/auth/analytics.readonly',
+                       'https://www.googleapis.com/auth/webmasters.readonly',
+                       'https://www.googleapis.com/auth/gmail.send']
+        self.OAUTH_SCOPE = OAUTH_SCOPE
+                   
+        # Redirect URI for installed apps4/4qWAzJo6NbPR-q2M42BbD7oYAFD4mr-mSgoH4OoSID0
+        self.REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+    # Run through the OAuth flow and retrieve credentials
+    def get_code(self):
+        flow = OAuth2WebServerFlow(self.CLIENT_ID, self.CLIENT_SECRET,
+                                   self.OAUTH_SCOPE, self.REDIRECT_URI)
+        authorize_url = flow.step1_get_authorize_url()
+        print('Go to the following link in your browser: ' + authorize_url)
+        #取り消したい場合は、適当な文字を入れて、実行を終わらせ。再度セルの実行をし、文字列を入れなおす
+        return auth.getpass.getpass()
+    
+    
+    def build_service(self, code):
+        flow = OAuth2WebServerFlow(self.CLIENT_ID, self.CLIENT_SECRET,
+                                   self.OAUTH_SCOPE, self.REDIRECT_URI)
+        credentials = flow.step2_exchange(code.strip())
+        # Create an httplib2.Http object and authorize it with our credentials
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+        ga4 = build('analytics', 'v4', http=http)
+        ga3 = build('analytics', 'v3', http=http)
+        return ga3, ga4
+    
+    
+    def ret2DataFrame(self, reports):
+      for report in reports:
+        dim_names = [x.replace("ga:","") for x in
+                report.get("columnHeader").get("dimensions")]
+        mtr_names = [x['name'].replace("ga:","") for x in 
+                report.get("columnHeader").get("metricHeader").get("metricHeaderEntries")]
+        mtr_dtypes = [x['type'].replace("ga:","") for x in 
+                report.get("columnHeader").get("metricHeader").get("metricHeaderEntries")]
+        mydic = {'STRING': str, "INTEGER": np.int, "FLOAT": np.float}
+        dim_dtypes = [str for _ in range(len(dim_names))]
+        mtr_dtypes = [mydic[x] for x in mtr_dtypes]
+        dim_ind = [x['dimensions'] for x in report['data']['rows']]
+        mtr_dat = np.array([x['metrics'][0]['values'] for x in report['data']['rows']])
+        yield pd.concat([
+            pd.DataFrame(dim_ind, columns=dim_names), 
+            pd.DataFrame(mtr_dat.astype(int),columns=mtr_names)], axis=1)
+    
+
+    
+    def getData(self, service, requests, nextPageToken=None):
+      body = {}
+      body["reportRequests"] = requests
+      #print(body)
+      ret = service.reports().batchGet(body=body).execute()
+      rowCount = ret['reports'][0]['data']['rowCount']
+      if not nextPageToken: print(rowCount) 
+      yield from ret2DataFrame(ret['reports'])
+      if 'nextPageToken' not in ret['reports'][0]:
+        return 
+      else:
+        nextPageToken = int(ret['reports'][0]['nextPageToken'])
+        requests[0]['pageSize'] = 10000
+        requests[0]['pageToken'] = str(nextPageToken) #need to be STRING!
+        #while nextPageToken + 10000 < rowCount:
+        #  new_req = req.copy()
+        #  new_req['pageToken'] = nextPageToken + 10000 
+        #  requests.append(new_req)
+        #  nextPageToken = nextPageToken + 10000
+        #  print("nextPageToken:{}".format(nextPageToken))
+        print("nextPageToken:{}".format(nextPageToken))
+        yield from getData(service, requests, nextPageToken)
 
 
-def ret2DataFrame(reports):
-  for report in reports:
-    dim_names = [x.replace("ga:","") for x in
-            report.get("columnHeader").get("dimensions")]
-    mtr_names = [x['name'].replace("ga:","") for x in 
-            report.get("columnHeader").get("metricHeader").get("metricHeaderEntries")]
-    mtr_dtypes = [x['type'].replace("ga:","") for x in 
-            report.get("columnHeader").get("metricHeader").get("metricHeaderEntries")]
-    mydic = {'STRING': str, "INTEGER": np.int, "FLOAT": np.float}
-    dim_dtypes = [str for _ in range(len(dim_names))]
-    mtr_dtypes = [mydic[x] for x in mtr_dtypes]
-    dim_ind = [x['dimensions'] for x in report['data']['rows']]
-    mtr_dat = np.array([x['metrics'][0]['values'] for x in report['data']['rows']])
-    yield pd.concat([
-        pd.DataFrame(dim_ind, columns=dim_names), 
-        pd.DataFrame(mtr_dat.astype(int),columns=mtr_names)], axis=1)
-
-
-def getData(ga, requests, nextPageToken=None):
-  body = {}
-  body["reportRequests"] = requests
-  #print(body)
-  ret = ga.reports().batchGet(body=body).execute()
-  rowCount = ret['reports'][0]['data']['rowCount']
-  if not nextPageToken: print(rowCount) 
-  yield from ret2DataFrame(ret['reports'])
-  if 'nextPageToken' not in ret['reports'][0]:
-    return 
-  else:
-    nextPageToken = int(ret['reports'][0]['nextPageToken'])
-    requests[0]['pageSize'] = 10000
-    requests[0]['pageToken'] = str(nextPageToken) #need to be STRING!
-    #while nextPageToken + 10000 < rowCount:
-    #  new_req = req.copy()
-    #  new_req['pageToken'] = nextPageToken + 10000 
-    #  requests.append(new_req)
-    #  nextPageToken = nextPageToken + 10000
-    #  print("nextPageToken:{}".format(nextPageToken))
-    print("nextPageToken:{}".format(nextPageToken))
-    yield from getData(ga, requests, nextPageToken)
+    def get_template(view_id):
+        return {
+            'viewId': view_id, 
+            'dateRanges': [{'startDate': 'yesterday', 'endDate': 'yesterday'}],
+            'metrics': [{'expression': 'ga:pageviews'},{'expression': 'ga:users'}],
+            #'dimensions': [{'name':'ga:channelGrouping'},{'name':'ga:dimension6'}]}
+            'dimensions': [{'name':'ga:channelGrouping'}]
+        }
